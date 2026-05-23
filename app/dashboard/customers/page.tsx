50b36be98e9cc8b264dd41dda46286cd1c7549e3
Robert McDowell <doodersrage@gmail.com>
2026-05-23 15:47:01 -0400

    meta data

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next/dist/lib/metadata/types/metadata-interface';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
 export default function Page() {
   return <p>Customers Page</p>;
 }
